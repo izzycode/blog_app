a368366245fdfca722b0e3350053a5a07771fca2
izzycode <marcemc@yahoo.com>
2016-11-14 18:00:24 -0500

    completed 1 on 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  get 'welcome/home'
+  get 'posts', to: "posts#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
